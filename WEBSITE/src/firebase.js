f78896be2591a3ffb64792bb0dc5c3e0ad3ac5e7
Mihir Kamat <kamatmihir03@gmail.com>
2026-08-28 17:19:43 +0530

    Security: extract Firebase config to environment variables

--- WEBSITE/src/firebase.js
+++ WEBSITE/src/firebase.js
@@ -3,14 +3,14 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAjMqJNOTQDt5GYdxVCMIAP5xquDFLvHGg",
-  authDomain: "caregrid-eb0a0.firebaseapp.com",
-  databaseURL: "https://caregrid-eb0a0-default-rtdb.firebaseio.com",
-  projectId: "caregrid-eb0a0",
-  storageBucket: "caregrid-eb0a0.firebasestorage.app",
-  messagingSenderId: "307776348499",
-  appId: "1:307776348499:web:4d82f1e22486f463c0c1a6",
-  measurementId: "G-ZTVP7TKLQR"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  databaseURL: import.meta.env.VITE_FIREBASE_DATABASE_URL,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
